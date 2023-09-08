df7cfd117960a7a56d4f769a98ffa5dcd6ebc481
Germar Bunda <123885878+Germar22@users.noreply.github.com>
2023-09-09 01:19:52 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,10 +1,19 @@
-function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
-
-  let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
-}
-
-document.getElementById('sumButton').addEventListener("click", displaySum);
-
+function displaySum() { 
+   let firstNum = Number(document.getElementById('firstNum').innerHTML) 
+   let secondNum = Number(document.getElementById('secondNum').innerHTML) 
+  
+   let total = firstNum + secondNum; 
+   document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ; 
+ } 
+  
+ document.getElementById('sumButton').addEventListener("click", displaySum); 
+  
+ function displaydif() { 
+   let firstNum = Number(document.getElementById('firstNum').innerHTML) 
+   let secondNum = Number(document.getElementById('secondNum').innerHTML) 
+  
+   let total = firstNum - secondNum; 
+   document.getElementById("answer2").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ; 
+ } 
+  
+ document.getElementById('difButton').addEventListener("click", displaydif);
